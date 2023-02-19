480592158173088ded89e6206e6e47eecbd8eea9
Tom Archer <tomarcher@toms-mbp-3.home>
2023-02-19 20:14:43 +0000

    Add favicon and fix mobile view

--- src/components/social-icons/SocialIcons.tsx
+++ src/components/social-icons/SocialIcons.tsx
@@ -2,11 +2,17 @@ import React from 'react'
 import styles from "./SocialIcons.module.scss"
 
 const SocialIcons = () => {
+  const Icon = ({ iconPath, linkPath }: { iconPath: string, linkPath: string }) => {
+    return (
+      <img src={iconPath} alt="" onClick={() => window.open(linkPath)} />
+    )
+  }
+
   return (
     <div className={styles.container}>
-      <img src="icons/github.svg" alt="" />
-      <img src="icons/linkedin.svg" alt="" />
-      <img src="icons/mail.svg" alt="" />
+      <Icon iconPath="icons/github.svg" linkPath="https://github.com/tomarcher101" />
+      <Icon iconPath="icons/linkedin.svg" linkPath="https://www.linkedin.com/in/tom-archer-229598a5/" />
+      <Icon iconPath="icons/mail.svg" linkPath="mailto:thomastarcher@icloud.com" />
     </div>
   )
 }
