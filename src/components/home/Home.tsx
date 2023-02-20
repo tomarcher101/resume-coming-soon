fdbe49322fd47dcdebcafaa6f92c0304731e6b6a
Tom Archer <tomarcher@toms-mbp-3.home>
2023-02-20 00:44:20 +0000

    Change transition time

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -7,19 +7,14 @@ import styles from "./Home.module.scss"
 const Home = () => {
   const [isMouseOverAvatar, setIsMouseOverAvatar] = useState(false)
   const className = `${styles.container} ${isMouseOverAvatar ? styles.dark : styles.light}`
+  const h2Text = isMouseOverAvatar ? "Site is under construction." : "Software Developer";
 
   return (
     <div className={className}>
       <Avatar setIsMouseOverAvatar={setIsMouseOverAvatar} />
       <div className={styles.description}>
         <h1>Tom Archer</h1>
-        {isMouseOverAvatar ? (
-          <>
-            <h2>Site is under construction.</h2>
-          </>
-        ) : (
-          <h2>Software Developer</h2>
-        )}
+        <h2>{h2Text}</h2>
       </div>
       <div className={styles.buttons}>
         <Button link={"./TomArcherCV20230216.pdf"}>View Resume</Button>
